02a3033a826eb7e1c133f6cddd0adf23cfce08c4
Alexandra Ljustina <acoljustina2004@gmail.com>
2023-03-06 11:42:55 +0100

    Final push
    The project ahs been finalised and is functional

---
+++ Formatiranje/Formatiranje/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Security;
+using System.Globalization;
+
+namespace uppercase_04._10._2020
+{
+    class Program
+    {
+        static string chageCase(string text, int b)
+        {
+            string txt;
+
+            switch (b)
+            {
+                case 1:
+                    txt = text.ToLower();
+                    break;
+                case 2:
+                    txt = text.ToUpper();
+                    break;
+                case 3:
+                    txt = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(text.ToLower());
+                    break;
+                default:
+                    return null;
+            }
+            return txt;
+        }
+        static void Main(string[] args)
+        {
+            string text = null;
+            int a;
+            Console.WriteLine("Izaberite radnju:\n1. Unesi tekst\n2. Promeni format\n0. Izadji");
+            do
+            {
+                a = int.Parse(Console.ReadLine());
+                if (a == 1)
+                {
+                    text = Console.ReadLine();
+                }
+                else if (a == 2)
+                {
+                    int b;
+                    do
+                    {
+                        Console.WriteLine("Izaberite radnju:\n1. Mala slova\n2. Velika slova\n3. Prva slova velika\n0. Nazad");
+                        b = int.Parse(Console.ReadLine());
+                        if (b == 1 || b == 2 || b == 3)
+                        {
+                            if (text == null)
+                            {
+                                Console.WriteLine("Tekst ne postoji");
+                                continue;
+                            }
+                            else
+                                Console.WriteLine(chageCase(text, b));
+                        }
+                        else if (b == 0)
+                            continue;
+
+                    } while (b != 0);
+
+
+                }
+                else
+                    continue;
+
+            } while (a != 0);
+
+
+        }
+    }
+}
---
+++ Formatiranje/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Formatiranje
+{
+    class Program
+    {
+        static string chageCase(string text, int i)
+        {
+            string txt;
+
+            switch (i)
+            {
+                case 1:
+                    txt = text.ToLower();
+                    break;
+                case 2:
+                    txt = text.ToUpper();
+                    break;
+                case 3:
+                    txt = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(text.ToLower());
+                    break;
+                default:
+                    return null;
+            }
+            return txt;
+        }
+        static void Main(string[] args)
+        {
+            string text = null;
+            int x;
+            Console.WriteLine("Choose action:\n1. Insert text\n2. Change format\n0. Exit");
+            do
+            {
+                x = int.Parse(Console.ReadLine());
+                if (x == 1)
+                {
+                    text = Console.ReadLine();
+                }
+                else if (x == 2)
+                {
+                    int i;
+                    do
+                    {
+                        Console.WriteLine("Choose action:\n1. Lower-case letters\n2. Upper-case letters\n3. Capital letters\n0. Go back");
+                        i = int.Parse(Console.ReadLine());
+                        if (i == 1 || i == 2 || i == 3)
+                        {
+                            if (text == null)
+                            {
+                                Console.WriteLine("Text does not exist");
+                                continue;
+                            }
+                            else
+                                Console.WriteLine(chageCase(text, i));
+                        }
+                        else if (i == 0)
+                            continue;
+
+                    } while (i != 0);
+
+
+                }
+                else
+                    continue;
+
+            } while (x != 0);
+
+
+        }
+    }
+}
